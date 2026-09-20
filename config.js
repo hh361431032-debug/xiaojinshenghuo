ca37a41a71dcc32fd7ac0d35f0be8a6f949c808e
hh361431032-debug <hh361431032@gmail.com>
2026-09-20 21:42:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG={
   app:{name:"小金生活",subtitle:"菜谱、采购、餐厅与地图，一处管理",themeColor:"#f7f5ef",accentColor:"#2f6f57"},
-  amap:{enabled:true,key:"",securityJsCode:"",version:"2.0",defaultCenter:[114.3055,30.5928],defaultZoom:12},
+  amap:{enabled:true,key:"56f19cd8cdbc289c8b8274424a2c0df9",securityJsCode:"28d92f4568e11c43291ba1bab419548",version:"2.0",defaultCenter:[114.3055,30.5928],defaultZoom:12},
   accounts:[
     {username:"admin",password:"123456",displayName:"主人"},
     {username:"wife",password:"123456",displayName:"家人"}
