01e06043fc628b78cb225550ac9dcd1ec43c4e45
hh361431032-debug <hh361431032@gmail.com>
2026-09-20 21:40:12 +0800

    建立小金生活 PWA

---
+++ config.js
@@ -0,0 +1,8 @@
+const APP_CONFIG={
+  app:{name:"小金生活",subtitle:"菜谱、采购、餐厅与地图，一处管理",themeColor:"#f7f5ef",accentColor:"#2f6f57"},
+  amap:{enabled:true,key:"",securityJsCode:"",version:"2.0",defaultCenter:[114.3055,30.5928],defaultZoom:12},
+  accounts:[
+    {username:"admin",password:"123456",displayName:"主人"},
+    {username:"wife",password:"123456",displayName:"家人"}
+  ]
+};
