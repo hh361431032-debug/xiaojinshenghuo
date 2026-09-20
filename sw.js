e43c639011778a54a14b83b8ec05f9b247b68bd5
hh361431032-debug <hh361431032@gmail.com>
2026-09-20 22:27:54 +0800

    更新缓存版本到v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="xiaojinshenghuo-v4";
+const CACHE="xiaojinshenghuo-v5";
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","config.js","manifest.json","assets/icon.svg"])))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
